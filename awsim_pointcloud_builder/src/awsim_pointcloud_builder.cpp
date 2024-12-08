5bda5cd4edbbb3466980f4f8d874e3d9a08dc382
kazushi67 <tengbenh64@gmail.com>
2024-12-09 04:32:11 +0900

    feat(.): add awsim_pointcloud_builder core pkg
    Signed-off-by: kazushi67 <tengbenh64@gmail.com>

---
+++ awsim_pointcloud_builder/src/awsim_pointcloud_builder.cpp
@@ -0,0 +1,10 @@
+#include "../include/awsim_pointcloud_builder/awsim_pointcloud_builder.hpp"
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  auto instance = std::make_shared<AwsimMapBuilder>();
+  rclcpp::spin(instance);
+  instance->public_transformPointCloud();
+  return 0;
+}
